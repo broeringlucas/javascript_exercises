ab7158a4620a1262efede9885516d9eb6e122424
broeringlucas <lucas10santos15@gmail.com>
2023-02-15 21:04:45 -0300

    novos exercicios

--- script.js
+++ script.js
@@ -263,3 +263,21 @@ while (i < populations1.length) {
 }
 
 console.log(percentage3);
+
+//Developer Skills & Editor Setup 
+
+//Coding Challenge 
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4]
+
+const printForecast = function (arr) {
+  let str = ''
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}ºC in ${i + 1} days... `
+  }
+  console.log(str)
+}
+
+printForecast(data1);
+printForecast(data2);
