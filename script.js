671ff73c5c6e8dc8f6778c7041bee940a7e3db75
broeringlucas <lucas10santos15@gmail.com>
2023-03-15 15:34:00 -0300

    novos exercicios2

--- script.js
+++ script.js
@@ -281,3 +281,84 @@ const printForecast = function (arr) {
 
 printForecast(data1);
 printForecast(data2);
+
+//Data Structure and Modern Operators 
+//Coding challange 
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimich',
+      'Horetzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4.0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hunneks'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 31.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+}
+
+//1
+const [players1, players2] = game.players;
+
+console.log(players1, players2);
+
+//2
+const [gk, ...fieldPlayers] = players1;
+
+console.log(gk);
+console.log(fieldPlayers);
+
+//3
+const allPlayers= [...players1, ...players2]
+console.log(allPlayers);
+
+//4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+//5
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+
+//6
+
+const printGoals = function(...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals('Davies', 'Muller', "Lewandowski", 'Kimmmich');
+printGoals(...game.scored);
+
+//7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
