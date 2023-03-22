ec1386b4edd58c1d6fd4bcbbcd93889a1b02dc68
broeringlucas <lucas10santos15@gmail.com>
2023-03-22 17:38:54 -0300

    novos exercises

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ printForecast(data1);
 printForecast(data2);
 
 //Data Structure and Modern Operators 
-//Coding challange 
+//Coding challange #1
 
 const game = {
   team1: 'Bayern Munich',
@@ -321,7 +321,7 @@ const game = {
   'Hunneks'],
   date: 'Nov 9th, 2037',
   odds: {
-    team1: 31.33,
+    team1: 1.33,
     x: 3.25,
     team2: 6.5,
   },
@@ -362,3 +362,27 @@ printGoals(...game.scored);
 //7
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
+
+//Coding challange #2
+console.log('-------- coding challange #2 --------');
+
+//1
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+};
+
+//2
+let average = 0 
+const odds = Object.values(game.odds);
+
+for (const odd of odds) {
+  average += odd;
+};
+average /= odds.length;
+console.log(average)
+
+//3 
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
