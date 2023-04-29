5aa830bbe446390ecc4838b0c9aa6a9d9f8039f9
broeringlucas <lucas10santos15@gmail.com>
2023-04-29 20:57:11 -0300

    abc

--- script.js
+++ script.js
@@ -547,16 +547,23 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log(`----------------------------------------`)
 // Coding challange #2
 
-const calcAverageHumanAge = function(ages) {
+const calcAverageHumanAge2 = function(ages) {
   const humanAges = ages.map(age => age <= 2 ? 2 * 
   age : 16 + age * 4);
   const adults = humanAges.filter(age => age >= 18);
-  console.log(humanAges);
-  console.log(adults);
 
   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length
 
   return average
 };
-const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-console.log(avg1)
+const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+console.log(avg1);
+
+const calcAverageHumanAge = ages =>
+  ages
+  .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+  .filter(age => age >= 18)
+  .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg2 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+console.log(avg2);
