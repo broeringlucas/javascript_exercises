6070f611c355eec875a4c963a8da3b81f375ea5b
broeringlucas <lucas10santos15@gmail.com>
2023-07-24 21:03:31 -0300

    commit

--- script.js
+++ script.js
@@ -621,3 +621,87 @@ console.log(dogs.filter(checkEatingOkay));
 // 8
 const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
 console.log(dogsSorted);
+
+// Object Oriented Programming (OOP)
+
+// 1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 10;
+  console.log(this.speed);
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+car1.accelerate();
+console.log(car1);
+car1.brake();
+
+// 2
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 10;
+    console.log(`${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const car3 = new CarCl("Ford", 120);
+car3.speedUS;
+car3.speedUS = 50;
+car3.accelerate();
+car3.brake();
+
+// 3
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+car4 = new EV("Tesla", 120, 23);
+car4.accelerate();
+car4.brake();
+car4.chargeBattery(90);
+console.log(car4.charge);
